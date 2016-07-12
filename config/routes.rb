314d93efcb009d795f5d0dff707095a75c765f5e
Gabriel Carpenedo <cbgabe@gmail.com>
2016-07-12 13:31:14 -0400

    Creates profile style

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :admin_users
   root to: 'pages#homepage'
 
   resources :people, only: :index
   resources :works, only: :index
-=======
 
   get 'style', to: 'pages#style', as: :style
->>>>>>> Creates style page
 end
