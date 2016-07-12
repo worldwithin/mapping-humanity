476a7e85d3bc83ebefceb907a41daed6f1f910f4
Gabriel Carpenedo <cbgabe@gmail.com>
2016-07-12 11:01:55 -0400

    Creates style page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   devise_for :admin_users
   root to: 'pages#homepage'
 
   resources :people, only: :index
   resources :works, only: :index
+=======
+
+  get 'style', to: 'pages#style', as: :style
+>>>>>>> Creates style page
 end
