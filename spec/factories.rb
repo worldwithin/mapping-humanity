18bf481439f465ce4a86ef85f75c596509ab8054
Patrick Müller <patrick.e.muller@gmail.com>
2016-07-12 15:43:35 -0300

    Adds Model for People

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,17 +1,18 @@
 FactoryGirl.define do
-  factory :category do
-    name "Category for People"
-    people true
-    works false
+  factory :person do
+    name "Person John Doe"
+    avatar ""
   end
 
   factory :category do
-    name "Category for Works"
-    people false
+    name "Category for People and Works"
+    people true
     works true
   end
 
   factory :admin_user do
-
+    name "Admin John Doe"
+    email "admin@example.com"
+    password "123123123"
   end
 end
---
+++ spec/models/person_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Person, type: :model do
+  describe 'Validations' do
+    it { should validate_presence_of :name }
+  end
+end
