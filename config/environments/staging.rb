95865eb0198b9833f19aec44ed5cb76997da432c
Patrick Müller <patrick.e.muller@gmail.com>
2016-07-14 20:21:05 -0300

    Adds missing action_controller.asset_host for staging.rb environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,10 +3,12 @@ require_relative "production"
 Rails.application.configure do
   if ENV.fetch("HEROKU_APP_NAME", "").include?("staging-pr-")
     config.action_mailer.asset_host = ("https://#{ENV["HEROKU_APP_NAME"]}.herokuapp.com")
+    config.action_controller.asset_host = ("https://#{ENV["HEROKU_APP_NAME"]}.herokuapp.com")
     config.action_mailer.default_url_options = { host: "#{ENV["HEROKU_APP_NAME"]}.herokuapp.com" }
     config.action_controller.default_url_options = { host: "#{ENV["HEROKU_APP_NAME"]}.herokuapp.com" }
   else
     config.action_mailer.asset_host = ("https://#{ENV["HEROKU_APP_NAME"]}-staging.herokuapp.com")
+    config.action_controller.asset_host = ("https://#{ENV["HEROKU_APP_NAME"]}-staging.herokuapp.com")
     config.action_mailer.default_url_options = { host: "#{ENV["HEROKU_APP_NAME"]}-staging.herokuapp.com"}
     config.action_controller.default_url_options = { host: "#{ENV["HEROKU_APP_NAME"]}-staging.herokuapp.com"}
   end
