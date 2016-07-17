0b80a0e967104d90901c56f65852497ae51dd881
Patrick Müller <patrick.e.muller@gmail.com>
2016-07-17 00:59:20 -0300

    Fixes problems with fog-aws on Heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,7 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
   if Rails.env.production? || Rails.env.staging?
     config.storage = :fog
---
+++ lib/fog.rb
@@ -0,0 +1,6 @@
+module Fog
+  # For some unknown reason this is necessary to fog-aws gem
+  # to work properly without requiring the fog gem
+  # Just did what is described here
+  # https://github.com/fog/fog/issues/3429
+end
